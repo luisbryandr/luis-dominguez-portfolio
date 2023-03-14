651dd8bf52e519af05ee42a86438608565aac95f
luisbryandr <luisbdominguezr@gmail.com>
2023-03-14 13:53:13 -0400

    built contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,26 @@
+import React from "react"
+
+
 export default function Contact() {
+    const user = {
+        resume: '',
+        linkedIn: 'https://www.linkedin.com/in/luisbdominguezr/',
+        gitHub: 'https://github.com/luisbryandr',
+    }
+
     return (
-        <div>Contact section</div>
+        <div id='Contact'>
+            <div>{user.resume && <a href={user.resume} download>Resume</a> }
+            </div>
+            <div className='footer-links'>
+                <a href='/'>Home</a>
+                <a href='#Projects'>Projects</a>
+                <a href='#About'>About</a>
+            </div>
+            <div>
+                <a href={user.linkedIn}>LinkedIn</a>
+                <a href={user.gitHub}>GitHub</a>
+            </div>
+        </div>
     )
 }
