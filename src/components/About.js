e641f1bb43669cc75fdca02cca2031cb55149f4e
luisbryandr <luisbdominguezr@gmail.com>
2023-03-14 16:48:39 -0400

    edited typo in bio

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import profilePic from '../assets/Portfolio-Profile-Pic.jpg'
 export default function About() {
     const userInfo = {
         profile_pic: profilePic,
-        bio: "I live by the idea that everything and everyone share a connection. Therefore I strive to write meaningful, well thoughtout out programs sourced from the heart.  My journey began as a biology major with a passion for science, creativity and critical thinking.  I have since discovered the power of code and love the fact I can incorporate my passions in a way that is magical and useful to everyday users. I am skilled in HTML, CSS, JS, React.js, Agile development, git version control.  I am also  familiar with Node.js, Express, RDBs, Postgres, SQL, Docker, Jira, Figma.",
+        bio: "I live by the idea that everything and everyone share a connection. Therefore I strive to write meaningful, well thoughtout programs sourced from the heart.  My journey began as a biology major with a passion for science, creativity and critical thinking.  I have since discovered the power of code and love the fact that I can incorporate my passions in a way that is magical and useful to everyday users. I am skilled in HTML, CSS, JS, React.js, Agile development, git version control.  I am also  familiar with Node.js, Express, RDBs, Postgres, SQL, Docker, Jira, Figma.",
     }
 
     return (
