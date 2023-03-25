8fd13086fb7c778c4a0bd85f52091ae4c0d684ce
luisbryandr <luisbdominguezr@gmail.com>
2023-03-24 21:02:46 -0400

    re ordering projects around for more relevance and added content to about statement

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,16 +7,16 @@ export default function Projects() {
 
     const userProjects = [
         {
-            title: 'Water My Plants', 
-            description: 'Ensuring that all your plants are consistently watered is actually pretty difficult. Water My Plants is an app that helps to solve those problems. With an easy to use interface for creating a plant watering schedule tailored to each individual plant, "Water My Plants" will remind users when it is time to feed that foliage and quench your plants', // describe what the application/project does and why its useful
+            title: 'Favorite-Teams-Tracker', 
+            description: `If you're like me and wanted a simple app to check on your favorite team's schedule then you are in luck because that is exactly what this app does.  I wanted a place where I could quickly check my team's upcoming games so I can add them to my schedule and enjoy watching them.  Using ESPN or other providers proved to be too clunky, too broad and complex, therefore I built this app to provide me with the most relevant information I was looking for regarding my teams.  Hope you enjoy its simplicity and specificity.`,// describe what the application/project does and why its useful
             contributions: [    // list how your contributions to the project
-                'Designed a responsive interface that assisted users in creating a plant watering schedule tailored for individual plants', 
-                'Developed a log-in page and functionality using Yup & Axios for form schema validation allowing user app access', 
-                'Instituted an edit page for users that facilitated plants list customization using React.js and Styled Components', 
+                'Designed the User Interface using Figma and styled it using Styled components ', 
+                'Built a React.js app, into a responsive and fluid functional page', 
+                'Incorporated a public sports API in order to get real time data', 
             ],
-            gitHub: 'https://github.com/Build-Week-Water-My-Plants-05/front-end', // add a link to repo here (leave blank to not display icon)
-            deployed_site: 'https://front-end-inky-two.vercel.app/',
-            background_image: "https://cdn.sanity.io/images/y346iw48/production/a59cbb8951fa663a7ffeef5324e1cf7037a70b57-4000x2250.jpg?w=884&auto=format", //if you have a deployed site then feel free to add it (leave blank to not display icon)
+            gitHub: 'https://github.com/luisbryandr/favorite-sports-teams', // add a link to repo here (leave blank to not display icon)
+            deployed_site: 'https://favorite-sports-teams.pages.dev/', //if you have a deployed site then feel free to add it (leave blank to not display icon)
+            background_image: "https://img2.cgtrader.com/items/2023706/025baf0f38/50-football-teams-3d-logo-collection-pack-3d-model.jpg",
         },
         {
             title: 'CoderHeroes ', 
@@ -44,16 +44,16 @@ export default function Projects() {
             background_image: "https://www.ourmindfullife.com/wp-content/uploads/2020/06/Motivation-quotes-for-Wednesday-ourmindfullife.com-1.jpg.webp",
         },
         {
-            title: 'Favorite-Teams-Tracker', 
-            description: `If you're like me and wanted a simple app to check on your favorite team's schedule then you are in luck because that is exactly what this app does.  I wanted a place where I could quickly check my team's upcoming games so I can add them to my schedule and enjoy watching them.  Using ESPN or other providers proved to be too clunky, too broad and complex, therefore I built this app to provide me with the most relevant information I was looking for regarding my teams.  Hope you enjoy its simplicity and specificity.`,// describe what the application/project does and why its useful
+            title: 'Water My Plants', 
+            description: 'Ensuring that all your plants are consistently watered is actually pretty difficult. Water My Plants is an app that helps to solve those problems. With an easy to use interface for creating a plant watering schedule tailored to each individual plant, "Water My Plants" will remind users when it is time to feed that foliage and quench your plants', // describe what the application/project does and why its useful
             contributions: [    // list how your contributions to the project
-                'Designed the User Interface using Figma and styled it using Styled components ', 
-                'Built a React.js app, into a responsive and fluid functional page', 
-                'Incorporated a public sports API in order to get real time data', 
+                'Designed a responsive interface that assisted users in creating a plant watering schedule tailored for individual plants', 
+                'Developed a log-in page and functionality using Yup & Axios for form schema validation allowing user app access', 
+                'Instituted an edit page for users that facilitated plants list customization using React.js and Styled Components', 
             ],
-            gitHub: 'https://github.com/luisbryandr/favorite-sports-teams', // add a link to repo here (leave blank to not display icon)
-            deployed_site: 'https://favorite-sports-teams.pages.dev/', //if you have a deployed site then feel free to add it (leave blank to not display icon)
-            background_image: "https://img2.cgtrader.com/items/2023706/025baf0f38/50-football-teams-3d-logo-collection-pack-3d-model.jpg",
+            gitHub: 'https://github.com/Build-Week-Water-My-Plants-05/front-end', // add a link to repo here (leave blank to not display icon)
+            deployed_site: 'https://front-end-inky-two.vercel.app/',
+            background_image: "https://cdn.sanity.io/images/y346iw48/production/a59cbb8951fa663a7ffeef5324e1cf7037a70b57-4000x2250.jpg?w=884&auto=format", //if you have a deployed site then feel free to add it (leave blank to not display icon)
         },
     ]
 
